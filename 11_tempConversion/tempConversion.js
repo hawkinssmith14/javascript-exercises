9bada7c28fbdc847f04bf5601b2f0ab8d1efadfa
Hawkins Smith <hawkins.smith0@gmail.com>
2026-01-19 10:51:59 +1000

    Converts from Celcius to Fahrenheit and vice versa

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let notRounded = (tempInFahrenheit - 32) * 5/9;
+  return Math.round(notRounded * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  let notRounded = tempInCelcius * (9/5) + 32;
+  return Math.round(notRounded * 10) / 10;
 };
 
 // Do not edit below this line
